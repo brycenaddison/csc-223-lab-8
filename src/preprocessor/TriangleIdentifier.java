44d2d78ce9a5c75d34a6bd989ae1fc9dd0dd8e46
Brycen Addison <addibr0@furman.edu>
2022-11-30 16:39:59 -0500

    implemented ComputeTriangles() in TriangleIdentifier
    Passes all tests

--- src/preprocessor/TriangleIdentifier.java
+++ src/preprocessor/TriangleIdentifier.java
@@ -1,10 +1,6 @@
 package preprocessor;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 import exceptions.FactException;
 import geometry_objects.Segment;
@@ -12,12 +8,12 @@ import geometry_objects.Triangle;
 
 public class TriangleIdentifier
 {
-	protected Set<Triangle>         _triangles;
-	protected Map<Segment, Segment> _segments;
+	protected Set<Triangle> _triangles;
+	protected Segment[] _segments;
 
 	public TriangleIdentifier(Map<Segment, Segment> segments)
 	{
-		_segments = segments;
+		_segments = segments.keySet().toArray(new Segment[0]);
 	}
 
 	/*
@@ -37,6 +33,49 @@ public class TriangleIdentifier
 
 	private void computeTriangles()
 	{
-		// TODO
+		int n = _segments.length;
+
+		if (n < 3) return;
+
+		int[] indices = new int[]{0, 1, 2};
+
+		while (true) {
+			processTriangle(indices);
+
+			int i = incrementable(indices, n);
+
+			// all combinations processed
+			if (i == -1) return;
+
+			increment(indices, i);
+		}
+	}
+
+	private void increment(int[] indices, int i) {
+		indices[i] += 1;
+		for (int j = i + 1; j < 3; j++) {
+			indices[j] = indices[j - 1] + 1;
+		}
+	}
+
+	// return index of index that can be incremented, -1 if none found
+	private int incrementable(int[] indices, int n) {
+		for (int i = 2; i >= 0; i--) {
+			if (indices[i] != i + n - 3) return i;
+		}
+		return -1;
+	}
+
+	private void processTriangle(int[] indices) {
+		List<Segment> segments = new ArrayList<>();
+
+		segments.add(_segments[indices[0]]);
+		segments.add(_segments[indices[1]]);
+		segments.add(_segments[indices[2]]);
+
+		try {
+			Triangle t = new Triangle(segments);
+			_triangles.add(t);
+		} catch (FactException e) {}
 	}
 }
