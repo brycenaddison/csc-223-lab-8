7f412170621c3884239c148d099c4f899fd6f47d
mandatory-username <106693160+hannaking@users.noreply.github.com>
2022-11-30 15:31:15 -0500

    added equivalence files

---
+++ src/utilities/eq_classes/EquivalenceClasses.java
@@ -0,0 +1,111 @@
+package utilities.eq_classes;
+
+/**
+ * ArrayList of LinkedEquivalenceClasses, all with the same comparator.
+ * LEC = LinkedEquivalenceClass
+ * 
+ * @author James, Ian, Hanna
+ * @date 9/23
+ */
+import java.util.Comparator;
+import java.util.List;
+import java.util.ArrayList;
+import java.lang.String;
+
+public class EquivalenceClasses<T>
+{
+	Comparator<T> _comparator;
+	List<LinkedEquivalenceClass<T>> _rest;
+	
+	/**
+	 * constructor, initializes all instance variables
+	 * _rest set to new ArrayList
+	 * @param Comparator<T> c
+	 */
+	public EquivalenceClasses(Comparator<T> c)
+	{
+		_comparator = c;
+		_rest = new ArrayList<LinkedEquivalenceClass<T>>();
+	}
+	
+	/**
+	 * adds element to the proper LEC
+	 * null elements cannot be added
+	 * if no LEC exists for element, a new one is created and add is called again
+	 *    element will become the canonical of this new LEC
+	 * @param T element value to add to a LEC
+	 * @return boolean false if value is not added (null), true otherwise
+	 */
+	public boolean add(T element)
+	{
+		if (element == null) return false;
+		for (LinkedEquivalenceClass<T> c : _rest)
+		{
+			if (c.add(element)) return true;
+		}
+		_rest.add(new LinkedEquivalenceClass<T>(_comparator));
+		return add(element);
+	}
+	
+	/**
+	 * looks in the LEC (_rest and canonical) for target
+	 * @param T target value to search for
+	 * @return boolean true if target is found in any LEC, false otherwise
+	 */
+	public boolean contains(T target) {
+		for (LinkedEquivalenceClass<T> c : _rest)
+		{
+			if (c.contains(target)) return true;
+		}
+		return false;
+	}
+	
+	/**
+	 * sum the sizes of every value each LEC's _rest, and add one for non-null canonicals
+	 * @return int total number of elements in all of the LECs, including canonicals
+	 */
+	public int size()
+	{
+		int sum = 0;
+		for (LinkedEquivalenceClass<T> lec : _rest)
+		{
+			sum = sum + lec.size();
+			if (lec.canonical() != null) sum += 1;
+		}
+		return sum;
+	}
+	
+	/**
+	 * @return the size of the underlying List
+	 */
+	public int numClasses() { return _rest.size(); }
+	
+	/**
+	 * loop through the List looking for the LEC containing element
+	 * @param T element
+	 * @return int index of LEC which element is a member of, -1 if target is not present
+	 */
+	protected int indexOfClass(T element)
+	{
+		for (int i = 0; i < this.size(); i++)
+		{
+			if (_rest.get(i).contains(element)) return i;
+		}
+		return -1;
+	}
+	
+	/**
+	 * format: ""1 | null, 4, 2, null : 7 | null, 13, null"
+	 * @return String representation of EquivalenceClasses object
+	 */
+	public String toString()
+	{
+		if (_rest.isEmpty()) return "null | null, null";
+		String str = new String();
+		for (LinkedEquivalenceClass<T> c : _rest)
+		{
+			str = str + c.toString() + " : ";
+		}
+		return str.substring(0, str.length() - 3);
+	}
+}
---
+++ src/utilities/eq_classes/LinkedEquivalenceClass.java
@@ -0,0 +1,152 @@
+package utilities.eq_classes;
+
+/**
+ * Equivalence Class using a Linked List to store values equivalent to the canonical value
+ * @author James, Ian, Hanna
+ * @date 9/23
+ */
+import java.util.Comparator;
+
+public class LinkedEquivalenceClass<T> extends LinkedList<T>{
+	
+	 T _canonical;
+	 Comparator<T> _comparator;
+	 LinkedList<T> _rest;
+
+	 /**
+	  * constructor, initializes all attributes
+	  * @param Comparator<T> c
+	  */
+	public LinkedEquivalenceClass(Comparator<T> c)
+	{
+		_canonical = null;
+		_comparator = c;
+		_rest = new LinkedList<T>();
+	}
+	
+	/**
+	 * @return T _canonical
+	 */
+	public T canonical() { return _canonical; }
+	
+	/**
+	 * @return true if _rest is empty, false if not
+	 * does not consider canonical value
+	 */
+	public boolean isEmpty() { return _rest.isEmpty(); }
+	
+	/**
+	 * clears _rest and resets _canonical to null
+	 */
+	public void clear()
+	{
+		_rest.clear();
+		_canonical = null;
+	}
+	
+	/**
+	 * clears _rest
+	 */
+	public void clearNonCanonical()	{ _rest.clear(); }
+	
+	/**
+	 * @return int the number of elements in _rest
+	 */
+	public int size() { return _rest._size;	}
+	
+	/**
+	 * if canonical is null, adds element as the canonical and returns true
+	 * otherwise, 
+	 *    if element belongs adds element to _rest and returns true.
+	 *    if element does not belong, returns false.
+	 * @param T element value to add
+	 * @return false if element does not belong, true if element is successfully added as canonical or in _rest
+	 */
+	public boolean add(T element)
+	{
+		if (_canonical == null)
+		{
+			demoteAndSetCanonical(element);
+			return true;
+		}
+		if (belongs(element))
+		{
+			_rest.addToFront(element);
+			return true;
+		}
+		return false;
+	}
+	
+	/**
+	 * check if value is in _rest or canonical
+	 * @param T target value to look for in LinkedList
+	 * @return true if canonical is the target or if _rest contains the target, false otherwise
+	 */
+	public boolean contains(T target)
+	{
+		if (target == null) return _canonical == target || _rest.contains(target);
+		return _canonical.equals(target) || _rest.contains(target);
+	}
+	
+	/**
+	 * determine if target meets requirements to be in the equivalence class
+	 * @param T target value to evaluate in comparator to canonical
+	 * @return false if target is null, already in _rest, or 
+	 */
+	public boolean belongs(T target)
+	{
+		if (target == null) return false;
+		return (contains(target) || _comparator.compare(_canonical, target) == 0);
+	}
+	
+	/**
+	 * remove target value from _rest, does not include canonical
+	 * calls removeCanonical if target is the canonical value
+	 * @param T target value to remove
+	 * @return boolean true if remove was successful, false otherwise
+	 */
+	public boolean remove(T target)
+	{	
+		if (target.equals(_canonical)) return removeCanonical();
+		return _rest.remove(target);
+	}
+	
+	/**
+	 * reset canonical to null
+	 * @return boolean true if remove successful
+	 */
+	public boolean removeCanonical()
+	{
+		_canonical = null;
+		return true;
+	}
+	
+	/**
+	 * adds old canonical value to _rest, then sets _canonical to element
+	 * will not add null to _rest, but still sets new _canonical
+	 * @param T element new value for canonical
+	 * @return true if a value is demoted to _rest, false if only _canonical changes
+	 */
+	public boolean demoteAndSetCanonical(T element)
+	{
+		if (_canonical == null)
+		{
+			_canonical = element;
+			return false;
+		}
+		_rest.addToFront(_canonical);
+		_canonical = element;
+		return true;		
+	}
+	
+	/**
+	 * returns String representation of LinkedEquivalenceList
+	 * formatted link: 2 | null, 8, 6, 4, null
+	 * @return String representation of LinkedEquivalenceClass
+	 */
+	public String toString()
+	{
+		if (_canonical == null) return "null | " + _rest.toString();
+		return _canonical.toString() + " | " + _rest.toString();
+	}
+}
